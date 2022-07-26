0383e700cf987294d5a36d6075b04610b21078c4
sonnt <sonnt@biplus.com.vn>
2022-07-26 21:25:56 +0700

    Initial Commit

---
+++ CalculatorUI/CalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUIApp.swift
+//  CalculatorUI
+//
+//  Created by son on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
